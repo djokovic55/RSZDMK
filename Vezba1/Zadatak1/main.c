f2ff00762651704fffa4140c652c71d47d178e75
djokovic55 <djokovic64@gmail.com>
2021-03-03 08:58:26 +0100

    Initial code added

--- Vezba1/Zadatak1/main.c
+++ Vezba1/Zadatak1/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
 	int main()
 	{
-		printf("Hello World");
-		return 0;
-	}
+		int a = 5;
+		int b = 4;
+		int c;
+	if(a > b)
+		c = a - b;
+	else
+		c = b - a;
+	printf("Result: %d\n", c);
+
+	return 0;
+}
